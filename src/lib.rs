f46d1fd2f5fb04398879160506b7a36ecdfaccb2
Abinoam Praxedes Marques Junior <abinoam@gmail.com>
2020-07-01 09:15:28 -0300

    Add self.[] method from rutie side

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ methods!(
     MatrixRs,
     _itself,
 
-    fn pub_reverse(input: RString) -> RString {
+    fn pub_self_brackets(input: RString) -> RString {
         let ruby_string = input.
           map_err(|e| VM::raise_ex(e) ).
           unwrap();
@@ -28,7 +28,7 @@ methods!(
 #[no_mangle]
 pub extern "C" fn Init_matrix_rs() {
     Class::new("MatrixRs", None).define(|itself| {
-        itself.def_self("reverse", pub_reverse);
+        itself.def_self("[]", pub_self_brackets);
     });
 }
 
