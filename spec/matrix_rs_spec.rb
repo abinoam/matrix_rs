62968e6f5e16b4ed6122b3cb5f6a175598e1e865
Abinoam Praxedes Marques Junior <abinoam@gmail.com>
2020-06-30 21:42:50 -0300

    Switch from RSpec to Minitest
    ... so we can reuse the Matrix gem test suite.

--- spec/matrix_rs_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe MatrixRs do
-  it "has a version number" do
-    expect(MatrixRs::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
-end
