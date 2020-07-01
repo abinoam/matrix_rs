3b610b693bcf805f3bd1b3a29b4e62504c6fa5fa
Abinoam Praxedes Marques Junior <abinoam@gmail.com>
2020-07-01 09:23:51 -0300

    Initialize rutie matrix_rs from Ruby MatrixRs

--- lib/matrix_rs.rb
+++ lib/matrix_rs.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 require "matrix_rs/version"
+require "rutie"
 
 # MatrixRs is a Rust extension replacement
 #   for Ruby standard library Matrix
 #
 class MatrixRs
-  class Error < StandardError; end
-  # Your code goes here...
+  Rutie.new(:matrix_rs).init "Init_matrix_rs", __dir__
 end
