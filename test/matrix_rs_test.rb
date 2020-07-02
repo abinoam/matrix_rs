dc8494ab632352c6c335c09fd34b1cfe772ce3fe
Abinoam Praxedes Marques Junior <abinoam@gmail.com>
2020-07-01 22:13:54 -0300

    Add MatrixRs.empty test
    Currently MatrixRs.empty is just half way implemented.
    It's returning an Array. Not a MatrixRs instance.

--- test/matrix_rs_test.rb
+++ test/matrix_rs_test.rb
@@ -6,4 +6,11 @@ class MatrixRsTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::MatrixRs::VERSION
   end
+
+  # Temporariy returning Arrays
+  def test_matrix_rs_empty_2_0
+    assert_equal ::MatrixRs.empty(2, 0), [[], []]
+    assert_equal ::MatrixRs.empty(2, 3), [[0, 0, 0], [0, 0, 0]]
+    assert_equal ::MatrixRs.empty(3, 2), [[0, 0], [0, 0], [0, 0]]
+  end
 end
