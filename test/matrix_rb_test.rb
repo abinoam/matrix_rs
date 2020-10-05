b49b330f22d4642a724948935f3739e4b5703452
Abinoam Praxedes Marques Junior <abinoam@gmail.com>
2020-10-05 04:01:49 -0300

    Add switch to opt in/out Ruby Matrix test suite

--- test/matrix_rb_test.rb
+++ test/matrix_rb_test.rb
@@ -2,5 +2,7 @@
 
 require "test_helper"
 
-# Run the Matrix gem test suite against MatrixRs
-require_relative "../vendor/matrix/test/matrix/test_matrix.rb"
+if ENV['RUN_RUBY_MATRIX_TEST_SUITE'] == 'true'
+  # Run the Matrix gem test suite against MatrixRs
+  require_relative "../vendor/matrix/test/matrix/test_matrix.rb"
+end
